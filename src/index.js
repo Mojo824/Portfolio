c6a69cf8f50952c4b6948c096bc5341e6f37b530
Mojo824 <insane.mjoshi@gmail.com>
2025-07-18 12:12:10 +0530

    wiped

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import initScrollReveal from "./scripts/scrollreveal";
-import initTiltEffect from "./scripts/tiltanimation";
-import { targetElements, defaultProps } from "./data/scrollrevealconfig";
-
-initScrollReveal(targetElements, defaultProps);
-initTiltEffect();
